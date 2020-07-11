6e8e97caa584da0aaaf9dafda399ebcf37cc3f68
emmaodia <odia.emma@gmail.com>
2020-07-11 03:21:43 +0100

    Cron job time update to 5 minutes.

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const sendSms = (phone, message) => {
-    const client = require('twilio')(accountSid, authToken);
-    client.messages
+  const client = require('twilio')(accountSid, authToken);
+  
+  client.messages
       .create({
          body: message,
          from: process.env.TWILIO_PHONE_NUMBER,
          to: phone
        })
       .then(message => console.log(message.sid));
-  }
+}
 
-cron.schedule('0 * * * *', () => {
+cron.schedule('*/5 * * * *', () => {
     console.log('SMS Sent successfully!');
     sendSms("+2347056355433", "This is from the server!");
-  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

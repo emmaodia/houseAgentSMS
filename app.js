82fca0a4081a4193cf2fd8e0e0ab35fdec132b4b
emmaodia <odia.emma@gmail.com>
2020-07-11 02:09:09 +0100

    Cron job implemented on sendSMS method.

--- app.js
+++ app.js
@@ -8,21 +8,20 @@ const port = process.env.PORT || 3000;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-// const sendSms = (phone, message) => {
-//     const client = require('twilio')(accountSid, authToken);
-//     client.messages
-//       .create({
-//          body: message,
-//          from: process.env.TWILIO_PHONE_NUMBER,
-//          to: phone
-//        })
-//       .then(message => console.log(message.sid));
-//   }
+const sendSms = (phone, message) => {
+    const client = require('twilio')(accountSid, authToken);
+    client.messages
+      .create({
+         body: message,
+         from: process.env.TWILIO_PHONE_NUMBER,
+         to: phone
+       })
+      .then(message => console.log(message.sid));
+  }
 
-// sendSms("+2347056355433", "This is from the server, again.");
-
-cron.schedule('* * * * *', () => {
-    console.log('running a task every minute');
+cron.schedule('0 * * * *', () => {
+    console.log('SMS Sent successfully!');
+    sendSms("+2347056355433", "This is from the server!");
   });
 
 app.listen(port, () => {
